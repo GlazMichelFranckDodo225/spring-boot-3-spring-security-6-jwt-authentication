8c9222294ec48cf3cc3eb83faab81310262a410c
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-10-04 15:37:01 +0200

    Restriction of Endpoints “/api/v1/admin/**” and “/api/v1/management/**” to Admins and Managers

--- src/main/java/com/dgmf/config/HttpRequestSecurityConfig.java
+++ src/main/java/com/dgmf/config/HttpRequestSecurityConfig.java
@@ -10,12 +10,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static com.dgmf.entity.enums.Permissions.*;
+import static com.dgmf.entity.enums.Role.ROLE_ADMIN;
+import static com.dgmf.entity.enums.Role.ROLE_MANAGER;
+import static org.springframework.http.HttpMethod.*;
+
 // This Class tells Spring which Configuration to use for
 // binding all the JWT Token Process
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class HttpRequestSecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authenticationProvider;
 
@@ -37,6 +42,24 @@ public class SecurityConfig {
                         // authentication or JWT Token ==> All the Endpoints
                         // inside the "AuthController" are authorized
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        /* Management */
+                        // Access Roles : only Admin and Manager Roles
+                        .requestMatchers("/api/v1/management/**").hasAnyRole(
+                                ROLE_ADMIN.name(), ROLE_MANAGER.name()
+                            )
+                        // Operations Permissions : only Admin and Manager Rights
+                        .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
+                        .requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
+                        .requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
+                        .requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
+                        /* Administration */
+                        // Access Roles : only Admin Role
+                        .requestMatchers("/api/v1/admin/**").hasRole(ROLE_ADMIN.name())
+                        // Operations Permissions : only Admin Rights
+                        .requestMatchers(GET, "/api/v1/admin/**").hasAuthority(ADMIN_READ.name())
+                        .requestMatchers(POST, "/api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
+                        .requestMatchers(PUT, "/api/v1/admin/**").hasAuthority(ADMIN_UPDATE.name())
+                        .requestMatchers(DELETE, "/api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())
                         // Authentication is required for all the other Requests
                         .anyRequest().authenticated()
                 )
